85c04db644e80c5b91b4f924e8da5abe8545f4c5
CaiQiuYu <919273003@qq.com>
2020-04-24 18:17:52 +0800

    数据处理代码

--- tools/generate_result.py
+++ tools/generate_result.py
@@ -6,4 +6,15 @@ Created on 2020-04-24, 15:14
 import json
 
 result = json.load(open("../data/result.json"))
-print(len(result))
+sentences = json.load(open("../data/validate_data.json", encoding="utf-8"))
+index = 0
+count = 0
+for k, v in sentences.items():
+    if len(list(v)) == len(result[index]):
+        count += 1
+    print(len(list(v)), end="----")
+    print(k, end="----")
+    print(len(result[index]))
+
+    index += 1
+print(count)
